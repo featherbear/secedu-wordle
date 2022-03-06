ee807cec6e4b075492567a0a85fdabcaf5c440ba
Andrew Wong <andrew.j.wong@outlook.com>
2022-03-07 05:37:14 +1100

    Add site analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import './lib/logger'
 import App from './App.svelte'
 
+if (location.hostname !== 'localhost') {
+  window['fathom'] = {
+    q: [
+      ['set', 'siteId', 'NEQTU'],
+      ['trackPageview']
+    ]
+  }
+
+  let o = document.createElement('script')
+  o.async = true;
+  o.src = '//ss.featherbear.cc/tracker.js';
+  o.id = 'fathom-script';
+  
+  let m = document.getElementsByTagName('script')[0];
+  m.parentNode.insertBefore(o, m)
+}
+
 const app = new App({
   target: document.getElementById('app')
 })
