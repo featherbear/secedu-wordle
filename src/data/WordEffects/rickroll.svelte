22ceda5e772c8b2deca6b5e170f81f3f62bc6782
Andrew Wong <andrew.j.wong@outlook.com>
2022-03-07 08:07:37 +1100

    Fix mobile autoplay

--- src/data/WordEffects/rickroll.svelte
+++ src/data/WordEffects/rickroll.svelte
@@ -1,7 +1,27 @@
+<script>
+  var tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var player;
+  window["onYouTubeIframeAPIReady"] = function () {
+    player = new window["YT"].Player("rick-roll-iframe", {
+      events: {
+        onReady: onPlayerReady,
+      },
+    });
+  };
+  function onPlayerReady(event) {
+    player.playVideo();
+  }
+</script>
+
 <div>
   <iframe
+    id="rick-roll-iframe"
     title="hehe"
-    src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1&loop=1"
+    src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1&loop=1&enablejsapi=1"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;"
   />
@@ -21,13 +41,12 @@
 
     &::after {
       content: "";
-      background-color: rgba(255,255,255,0.2);
+      background-color: rgba(255, 255, 255, 0.2);
 
       width: 100%;
       height: 100%;
 
       position: absolute;
-
     }
 
     iframe {
@@ -35,8 +54,8 @@
       height: 100%;
       width: 100%;
 
-      filter: blur(10px);
-      transform: scale(1.5);
+      filter: blur(3px);
+      transform: scale(3.5);
     }
   }
 </style>
